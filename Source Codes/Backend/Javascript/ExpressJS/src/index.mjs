67dbf2ab337d0894a65c80d6767fe6045f0de9bd
Solomon <solomondanso2023@gmail.com>
2024-05-08 12:18:41 -0500

    Middleware Crusing

--- Source Codes/Backend/Javascript/ExpressJS/src/index.mjs
+++ Source Codes/Backend/Javascript/ExpressJS/src/index.mjs
@@ -1,8 +1,56 @@
-import express from "express"
+import express, { response } from "express"
 
 
 const app = express()
 app.use(express.json());
+
+
+const resolveIndexByUserId = (req, res, next) =>{
+
+    const {params:{id}, } = req;
+
+    const Id = parseInt(id)
+    
+    if(isNaN(id)){
+      return res.status(400).send("Id is not a number")
+    }
+
+    const findUserIndex = mockUsers.findIndex( (data)=>data.id===Id)
+
+        if(findUserIndex === -1){
+      return  res.status(404).send({msg:"User not found"})
+    }
+
+    //We can dynamically assign values to the req object, since we cannot pass data from one middleware to the other
+
+    req.findUserIndex = findUserIndex;
+    req.Id = Id;
+    next();
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 const PORT = process.env.PORT|| 3001;
@@ -11,9 +59,12 @@ const PORT = process.env.PORT|| 3001;
 
 
 
-app.get("/",(req,res)=>{
+app.get("/",
+(req,res)=>{
 res.status(200).send({msg:"Kofi Money"})
 })
+
+
 
 const mockUsers = [
     {
@@ -55,7 +106,6 @@ app.get("/api/users",(req,res)=>{
     
 
     return res.send({mockUsers})
-
     
 })
 
@@ -107,37 +157,9 @@ app.post("/api/users",(req,res)=>{
 })
 
 
-app.put("/api/users/:id",(req,res)=>{
-
-    const {body, params:{id}, } = req;
-
-    const Id = parseInt(id)
-    
-    if(isNaN(id)){
-      return res.status(400).send("Id is not a number")
-    }
-
-    //This will find the index of the object in the array where it id is equal to the id provided
-    const findUserIndex = mockUsers.findIndex( (data)=>data.id===Id)
-
-        if(findUserIndex === -1){
-      return  res.status(404).send({msg:"User not found"})
-    }
-/*
-localhost:3001/api/users/2
-return res.send(findUserIndex.toString())
-the index is 1
-
-return res.send(mockUsers[findUserIndex])
-the result is 
-{
-  "id": 2,
-  "fname": "Danso"
-}
-
-
-
- */
+app.put("/api/users/:id",resolveIndexByUserId,(req,res)=>{
+//Destructure the findUserIndex and Id from the request
+const {body,findUserIndex,Id} = req;
 
 const old = mockUsers[findUserIndex]
 mockUsers[findUserIndex] = {id:Id, ...body}
@@ -152,6 +174,54 @@ return res.send(wow)
     
 
 })
+
+
+app.patch("/api/users/:id",resolveIndexByUserId ,(req,res)=>{
+
+    const {body,findUserIndex} = req;
+
+    mockUsers[findUserIndex] = {...mockUsers[findUserIndex], ...body};
+
+ 
+    return res.send( mockUsers[findUserIndex] )
+
+
+})
+
+
+app.delete("/api/users/:id",(req, res)=>{
+
+    const {params:{id} } = req;
+    const Id = parseInt(id)
+    const uIndex = mockUsers.findIndex((data)=>data.id===Id);
+
+    if(uIndex<0){
+        return res.send({msg:"User not found"})
+    }
+
+    if(isNaN(Id)){
+        return res.send({msg:"Imvalid Id"})
+    } 
+/*
+Find the array of the element you want to delete
+const uIndex = mockUsers.findIndex((data)=>data.id===Id);
+
+Delete the array and make the count 1, otherwise it will delete all the 
+    mockUsers.splice(uIndex,1)
+
+
+
+*/
+    mockUsers.splice(uIndex,1)
+
+
+
+    return res.send(mockUsers)
+
+})
+
+
+
 
 
 
